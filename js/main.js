be00a9c5f742e820c53202ee5c5ebb42c7c40993
menybenatar <menybenatar@gmail.com>
2022-04-30 19:43:17 +0300

    change font and style on burrons

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
-const elCardBtns = document.querySelectorAll(".card-btn");
-elCardBtns.forEach((cardBtn) => {
-  cardBtn.addEventListener("mouseenter", onCardBtnHover);
-  cardBtn.addEventListener("mouseleave", onCardBtnOut);
-});
-function onCardBtnHover(ev) {
-  var spanNav = ev.target.dataset.span;
-  ev.toElement.firstElementChild.innerText = "➡";
-}
+// const elCardBtns = document.querySelectorAll(".card-btn");
+// elCardBtns.forEach((cardBtn) => {
+//   cardBtn.addEventListener("mouseenter", onCardBtnHover);
+//   cardBtn.addEventListener("mouseleave", onCardBtnOut);
+// });
+// function onCardBtnHover(ev) {
+//   var spanNav = ev.target.dataset.span;
+//   ev.toElement.firstElementChild.innerText = "➡";
+// }
 
-function onCardBtnOut(ev) {
-  var spanNum = ev.target.dataset.span;
-  document.querySelector(`.btn${spanNum} span`).innerText = "";
-}
+// function onCardBtnOut(ev) {
+//   var spanNum = ev.target.dataset.span;
+//   document.querySelector(`.btn${spanNum} span`).innerText = "";
+// }
