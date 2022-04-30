5500f5998e7780b5591a9bb6d0f80a836b3629e6
menybenatar <menybenatar@gmail.com>
2022-04-30 15:16:00 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const elCardBtns = document.querySelectorAll(".card-btn");
+elCardBtns.forEach((cardBtn) => {
+  cardBtn.addEventListener("mouseenter", onCardBtnHover);
+  cardBtn.addEventListener("mouseleave", onCardBtnOut);
+});
+function onCardBtnHover(ev) {
+  var spanNav = ev.target.dataset.span;
+  ev.toElement.firstElementChild.innerText = "➡";
+}
+
+function onCardBtnOut(ev) {
+  var spanNum = ev.target.dataset.span;
+  document.querySelector(`.btn${spanNum} span`).innerText = "";
+}
